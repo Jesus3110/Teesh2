a4af1f1c52e08f9bfd4d7892ec36735d5770766c
Jesus3110 <114785322+Jesus3110@users.noreply.github.com>
2025-05-28 12:43:00 -0600

    Ya quedo el chat para el server 2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-
+import vercel from '@astrojs/vercel/serverless';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [svelte()],
-
-    vite: {
-        plugins: [tailwindcss()]
-    }
+  output: 'server',
+  adapter: vercel(),
+  integrations: [svelte()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
