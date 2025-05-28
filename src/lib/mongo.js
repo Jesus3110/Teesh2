0f0b8106b3970d6520c2c00b84666afc15b3799e
Jesus3110 <114785322+Jesus3110@users.noreply.github.com>
2025-05-28 13:07:44 -0600

    Ya quedo el chat para el server 3.0

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -2,13 +2,12 @@ import { MongoClient } from "mongodb";
 
 const uri = import.meta.env.MONGO_URI;
 const client = new MongoClient(uri);
-
 let db;
 
 export async function getDb() {
   if (!db) {
     await client.connect();
-    db = client.db("chatbot"); // nombre de tu base local
+    db = client.db(); // sin nombre: se toma de la URI automáticamente
   }
   return db;
 }
